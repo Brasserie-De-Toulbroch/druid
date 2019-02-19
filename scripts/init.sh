c29322efa97f78591861442ca832b5ddd508bbe3
Blottiere Paul <blottiere.paul@gmail.com>
2019-02-19 07:57:55 +0100

    Remove unnecessary file

--- scripts/init.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-sqlite3 $1 <<END_SQL
-CREATE TABLE recipes(id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT, notes TEXT, final_vol INTEGER, mashing_water_vol INTEGER, mashing_wort_vol INTEGER, mashing_temp INTEGER, mashing_duration INTEGER, yeast TEXT)
-END_SQL
-
-sqlite3 $1 <<END_SQL
-CREATE TABLE malts(recipe_id INTEGER, name TEXT, ebc INTEGER, weight INTEGER, FOREIGN KEY(recipe_id) REFERENCES recipes(id) )
-END_SQL
