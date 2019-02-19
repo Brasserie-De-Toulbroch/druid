41d233910ea01964bc0f28883af87add55d5ac26
Blottiere Paul <blottiere.paul@gmail.com>
2019-02-19 07:53:43 +0100

    Add toHeight feature

--- src/app/volume.cpp
+++ src/app/volume.cpp
@@ -16,28 +16,34 @@ DruidVolume::DruidVolume() {
   _fermentor.diameter = 37.0;
   _fermentor.height = 50.0;
 
-  connect(_compute_sterilizer, &QPushButton::clicked, this,
-          &DruidVolume::compute_sterilizer);
-  connect(_compute_boiler, &QPushButton::clicked, this,
-          &DruidVolume::compute_boiler);
-  connect(_compute_fermentor, &QPushButton::clicked, this,
-          &DruidVolume::compute_fermentor);
+  connect(_sterilizer_to_volume, &QPushButton::clicked, this,
+          &DruidVolume::sterilizer_to_volume);
+  connect(_boiler_to_volume, &QPushButton::clicked, this,
+          &DruidVolume::boiler_to_volume);
+  connect(_fermentor_to_volume, &QPushButton::clicked, this,
+          &DruidVolume::fermentor_to_volume);
+  connect(_sterilizer_to_height, &QPushButton::clicked, this,
+          &DruidVolume::sterilizer_to_height);
+  connect(_boiler_to_height, &QPushButton::clicked, this,
+          &DruidVolume::boiler_to_height);
+  connect(_fermentor_to_height, &QPushButton::clicked, this,
+          &DruidVolume::fermentor_to_height);
 }
 
-void DruidVolume::compute_sterilizer() {
-  const float height_from_top = _height_sterilizer->value();
+void DruidVolume::sterilizer_to_volume() {
+  const float height_from_top = _value_sterilizer->value();
   const float volume = volume_in_l(_sterilizer, height_from_top);
   _lcd->display(QString::number(volume, 'f', 1));
 }
 
-void DruidVolume::compute_fermentor() {
-  const float height_from_top = _height_fermentor->value();
+void DruidVolume::fermentor_to_volume() {
+  const float height_from_top = _value_fermentor->value();
   const float volume = volume_in_l(_fermentor, height_from_top);
   _lcd->display(QString::number(volume, 'f', 1));
 }
 
-void DruidVolume::compute_boiler() {
-  const float height_from_top = _height_boiler->value();
+void DruidVolume::boiler_to_volume() {
+  const float height_from_top = _value_boiler->value();
   const float volume = volume_in_l(_boiler, height_from_top);
   _lcd->display(QString::number(volume, 'f', 1));
 }
@@ -52,4 +58,29 @@ float DruidVolume::volume_in_l(const Tun &tun,
   }
 
   return volume / 1000;
+}
+
+void DruidVolume::sterilizer_to_height() {
+  const float volume = _value_sterilizer->value();
+  const float height_from_top = height_in_cm(_sterilizer, volume);
+  _lcd->display(QString::number(height_from_top, 'f', 1));
+}
+
+void DruidVolume::boiler_to_height() {
+  const float volume = _value_boiler->value();
+  const float height_from_top = height_in_cm(_boiler, volume);
+  _lcd->display(QString::number(height_from_top, 'f', 1));
+}
+
+void DruidVolume::fermentor_to_height() {
+  const float volume = _value_fermentor->value();
+  const float height_from_top = height_in_cm(_fermentor, volume);
+  _lcd->display(QString::number(height_from_top, 'f', 1));
+}
+
+float DruidVolume::height_in_cm(const Tun &tun, const float volume) const {
+  const float surface = 3.14 * (tun.diameter / 2) * (tun.diameter / 2);
+  const float height = (volume * 1000) / surface;
+
+  return tun.height - height;
 }
--- src/app/volume.h
+++ src/app/volume.h
@@ -4,26 +4,28 @@
 
 #include "ui_volume.h"
 
-class DruidVolume: public QGroupBox, public Ui::DruidVolume
-{
-    Q_OBJECT
+class DruidVolume : public QGroupBox, public Ui::DruidVolume {
+  Q_OBJECT
 
-    public:
-        struct Tun
-        {
-            float diameter;
-            float height;
-        };
+ public:
+  struct Tun {
+    float diameter;  // cm
+    float height;    // cm
+  };
 
-        DruidVolume();
+  DruidVolume();
 
-    private:
-        void compute_sterilizer();
-        void compute_boiler();
-        void compute_fermentor();
-        float volume_in_l( const Tun &tun, float height_from_top ) const;
+ private:
+  void sterilizer_to_volume();
+  void boiler_to_volume();
+  void fermentor_to_volume();
+  void sterilizer_to_height();
+  void boiler_to_height();
+  void fermentor_to_height();
+  float volume_in_l(const Tun &tun, float height_from_top) const;
+  float height_in_cm(const Tun &tun, float volume) const;
 
-        Tun _sterilizer;
-        Tun _boiler;
-        Tun _fermentor;
+  Tun _sterilizer;
+  Tun _boiler;
+  Tun _fermentor;
 };
