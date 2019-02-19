81011eebc6232640f1f2aa92f6152bab0c4f8acb
Blottiere Paul <blottiere.paul@gmail.com>
2019-02-19 19:58:40 +0100

    Timer is blinking when terminated

--- src/app/timer.cpp
+++ src/app/timer.cpp
@@ -2,60 +2,70 @@
 
 #include <timer.h>
 
-DruidTimer::DruidTimer()
-{
-    setupUi( this );
+DruidTimer::DruidTimer() {
+  setupUi(this);
 
-    _lcd->setDigitCount(8);
+  _lcd->setDigitCount(8);
 
-    connect( _start, &QPushButton::clicked, this, &DruidTimer::start );
-    connect( _stop, &QPushButton::clicked, this, &DruidTimer::stop );
+  connect(_start, &QPushButton::clicked, this, &DruidTimer::start);
+  connect(_stop, &QPushButton::clicked, this, &DruidTimer::stop);
 }
 
-void DruidTimer::start()
-{
-    const int hours = _hour->value();
-    const int minutes = _minutes->value();
-    const int msecs = ( hours * 60 * 60 + minutes * 60 ) * 1000;
+void DruidTimer::start() {
+  const int hours = _hour->value();
+  const int minutes = _minutes->value();
+  const int msecs = (hours * 60 * 60 + minutes * 60) * 1000;
 
-    _lcd->display( DruidTimer::msecs_to_string( msecs ) );
-    QTimer::singleShot( 1000, this, &DruidTimer::update );
-    _timer.start( msecs );
-    _timer_stop = false;
+  _lcd->display(DruidTimer::msecs_to_string(msecs));
+  QTimer::singleShot(1000, this, &DruidTimer::update);
+  _timer.start(msecs);
+  _timer_stop = false;
 }
 
-void DruidTimer::update()
-{
-    if ( ! _timer_stop )
-    {
-        const int msecs = _timer.remainingTime();
-        _lcd->display( DruidTimer::msecs_to_string( msecs ) );
+void DruidTimer::update() {
+  if (!_timer_stop) {
+    const int msecs = _timer.remainingTime();
 
-        if ( msecs/1000 > 0 )
-        {
-            QTimer::singleShot(1000, this, &DruidTimer::update);
-        }
-        else
-        {
-            _timer.stop();
-        }
+    if (msecs / 1000 > 0) {
+      _default_palette = _lcd->palette();
+      _lcd->display(DruidTimer::msecs_to_string(msecs));
+    } else {
+      _lcd->display(DruidTimer::msecs_to_string(0.0));
+      _timer.stop();
+
+      if (_red) {
+        _lcd->setPalette(_default_palette);
+        _red = false;
+      } else {
+        QPalette palette;
+        palette.setColor(QPalette::Normal, QPalette::Foreground, Qt::red);
+        palette.setColor(QPalette::Normal, QPalette::Dark, Qt::red);
+        palette.setColor(QPalette::Normal, QPalette::Light, Qt::red);
+        palette.setColor(QPalette::Normal, QPalette::Background, Qt::red);
+        _lcd->setPalette(palette);
+        _red = true;
+      }
     }
+
+    QTimer::singleShot(1000, this, &DruidTimer::update);
+  } else {
+    _timer.stop();
+  }
 }
 
-void DruidTimer::stop()
-{
-    _timer_stop = true;
+void DruidTimer::stop() {
+  _timer_stop = true;
+  _lcd->setPalette(_default_palette);
 }
 
-QString DruidTimer::msecs_to_string( const int millisecondes )
-{
-    int secondes = millisecondes/1000;
-    int minutes = secondes/60;
-    const int hours = minutes/60;
+QString DruidTimer::msecs_to_string(const int millisecondes) {
+  int secondes = millisecondes / 1000;
+  int minutes = secondes / 60;
+  const int hours = minutes / 60;
 
-    minutes = minutes - hours * 60;
-    secondes = secondes - hours * 60 * 60 - minutes * 60;
+  minutes = minutes - hours * 60;
+  secondes = secondes - hours * 60 * 60 - minutes * 60;
 
-    const QTime time( hours, minutes, secondes );
-    return time.toString( "hh:mm:ss" );
+  const QTime time(hours, minutes, secondes);
+  return time.toString("hh:mm:ss");
 }
--- src/app/timer.h
+++ src/app/timer.h
@@ -5,20 +5,21 @@
 
 #include "ui_timer.h"
 
-class DruidTimer: public QGroupBox, public Ui::DruidTimer
-{
-    Q_OBJECT
+class DruidTimer : public QGroupBox, public Ui::DruidTimer {
+  Q_OBJECT
 
-    public:
-        DruidTimer();
+ public:
+  DruidTimer();
 
-    private:
-        void start();
-        void stop();
-        void update();
+ private:
+  void start();
+  void stop();
+  void update();
 
-        static QString msecs_to_string( int millisecondes );
+  static QString msecs_to_string(int millisecondes);
 
-        QTimer _timer;
-        bool _timer_stop = false;
+  QTimer _timer;
+  bool _timer_stop = false;
+  bool _red = false;
+  QPalette _default_palette;
 };
